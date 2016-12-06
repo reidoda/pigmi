121e50443785603876f0c45b295b70517cfd0840
Reid Oda <reid.oda@gmail.com>
2016-12-06 14:32:25 -0500

    more accurate start time

--- python/midi_clock.pyx
+++ python/midi_clock.pyx
@@ -20,21 +20,25 @@ def main():
     def send_start():
         midiout.send_message([250])
 
-    def send_midi_clock(tempo):
+    def send_midi_clock(tempo,start_time):
         cdef float period
         cdef double start_time, now, next_pulse_time
         start_time = time.time() # maybe change to absolute time later
+	started = False
         period = (60.0/tempo)/24.0
         lookahead = 0.004
     
         while True:
             now = time.time()
+	    if not started:
+                if start_time - now < lookahead:
+		    threading.Timer(start_time - time.time(), send_start, ()).start()
             next_pulse_time = start_time + (np.floor((now-start_time)/period) + 1) * period
             if next_pulse_time - now < lookahead:
                 threading.Timer(next_pulse_time - time.time(), send_pulse, ()).start()
                 time.sleep(lookahead*2)
             else:
-                time.sleep(0.001)
+                time.sleep(0.0005)
 
     def start_on_measure(tempo):
         threshold = 0.001
@@ -43,8 +47,7 @@ def main():
         next_measure_time = (np.floor(now/period) + 1) * period
         while next_measure_time - time.time() > threshold:
             time.sleep(0.001)
-#        midiout.send_message([250])
-        send_midi_clock(tempo)
+        send_midi_clock(tempo,next_measure_time)
 
     start_on_measure(120)
 
